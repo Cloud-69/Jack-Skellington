30dc887e77fb268141368dbd1b11131aa7bee5b8
Cowi <gabrielmoreiralecaille@gmail.com>
2020-10-22 10:24:32 -0300

    Falta hacerlo mas lindo y unit testing

--- src/paquete/Principal.java
+++ src/paquete/Principal.java
@@ -11,10 +11,10 @@ public class Principal {
         int ptoCrupier = 0;
         int otraCarta = 1;  
         int whoWon = 0; // 1 blackjack jugador - 2 blackjack crupier - 3 jugador normal - 4 crupier normal 
-                        // 5 jugador se pasa - 6 crupier se pasa
+                        // 5 jugador se pasa - 6 crupier se pasa - 7 empate
 
         while (whoWon == 0){                            // When someone wins, whoWon changes it value,
-            for (int i = 0; i <= 2; i++) {              // taking them out of the game
+            for (int i = 0; i < 2; i++) {              // taking them out of the game
                 ptoJugador += darCarta();
             }
             if (ptoJugador == 21){
@@ -25,17 +25,18 @@ public class Principal {
                 System.out.println("Sus puntos suman "+ptoJugador+
                 "\nQuiere otra carta?\n1 para si, 0 para no");
                 otraCarta = entrada.nextInt();
-                ptoJugador += darCarta();
+                if (otraCarta == 1){ptoJugador += darCarta();}
             }
             if (ptoJugador == 21){
+                System.out.println("Hiciste blackjack, pero si el crupier también hace blackjack, él ganará");
                 whoWon = 1;
-            }
-            if (ptoJugador > 21){
+            } else if (ptoJugador > 21){
                 whoWon = 5;
             }
 
-            
-            for (int i = 0; i <= 2; i++) {
+            if (whoWon == 0){
+                System.out.println("Se le darán cartas al crupier");
+                for (int i = 0; i < 2; i++) {
                 ptoCrupier += darCarta();
             }
             if (ptoCrupier == 21){
@@ -43,9 +44,20 @@ public class Principal {
             }
             while (ptoCrupier<=16 && ptoCrupier <= ptoJugador){
                 ptoCrupier+=darCarta();
+            }}
+
+            if (whoWon == 1){
+                System.out.println("Se le darán cartas al crupier");
+                for (int i = 0; i < 2; i++) {
+                ptoCrupier += darCarta();
+                    }
+                if (ptoCrupier == 21){
+                // BLACKJACK CRUPIER
+                whoWon = 2;
+                }
             }
 
-            whoWon = winLose(ptoJugador, ptoCrupier);
+            if (whoWon == 0){whoWon = winLose(ptoJugador, ptoCrupier);}
 
         }
 
@@ -63,10 +75,16 @@ public class Principal {
                 System.out.println("El crupier te ganó con "+ptoCrupier+" puntos, pero ánimo, tuviste "+ptoJugador+" puntos");
                 break;
             case 5:
-                System.out.println("Te pasaste de 21 puntos");
+                System.out.println("Te pasaste de 21 puntos, tuviste "+ptoJugador);
                 break;
             case 6:
-                System.out.println("El crupier se pasó de 21");
+                System.out.println("El crupier se pasó de 21, tuvo "+ptoCrupier);
+                break;
+            case 7:
+                System.out.println("Empataron, el crupier gana");
+                break;
+            default:
+                System.out.println("Disculpanos, ha ocurrido un error. Contacta a los developers para solucionarlo\nPuedes volver a iniciar el juego, posiblemente lo resuelva");
         }
         
     }
@@ -76,13 +94,19 @@ public class Principal {
         
         if (crupier > 21){
             return 6;
-        }
-        
-        if (player > crupier){
+        }else if (player > crupier && player <=21){
             return 3;
         }
-        else{
+        else if (crupier > player && crupier <=21){
             return 4;
+        }
+        else if(player > 21){
+            return 5;
+        } else if(crupier == player){
+            return 7;
+        }
+        else{
+            return 10;
         }
     }
 
@@ -94,7 +118,8 @@ public class Principal {
         int i = azar.nextInt(13);
         String[] palo = {"corazones","diamantes","pica","trébol"};
         int j = azar.nextInt(4);
-        System.out.println("\n"+numero[i] + " de " + palo[j]); // agrego un \n al principio para que sea más fácil ver el número
+        System.out.println("\n"+
+        numero[i] + " de " + palo[j]); // agrego un \n al principio para que sea más fácil ver el número
         i++;
         puntos = i;
         switch(i){
@@ -110,7 +135,8 @@ public class Principal {
 
     public static void main(String[] args){
         Principal p = new Principal();
-        p.iniciar();
         System.out.println("Bienvenido al juego, le daremos dos cartas");
+        p.iniciar();
+        
     }
 }
